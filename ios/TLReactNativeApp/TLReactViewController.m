17d55f3692da8b2c552f18078fcfc6568039e6a6
lichuanjun <lichuanjun@niwodai.net>
2018-12-18 19:05:16 +0800

    增加真机联调的配置

--- ios/TLReactNativeApp/TLReactViewController.m
+++ ios/TLReactNativeApp/TLReactViewController.m
@@ -28,8 +28,8 @@
                                                  }
                                              ]
                                      };
-    
-    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+//    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://10.15.233.171:8081/index.bundle?platform=ios"];
     RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                         moduleName:@"TLReactNativeApp"
                                                  initialProperties:propertiesDict
